e8baed13b79e374659ce01366d716bab27b8b48e
Ubuntu <ubuntu@ip-172-31-36-41.us-east-2.compute.internal>
2019-08-12 22:36:54 +0000

    roll the dice medium tutorial

--- dice-roller/src/App.js
+++ dice-roller/src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import one from "./assets/one.png";
+import two from "./assets/two.png";
+import three from "./assets/three.png";
+import four from "./assets/four.png";
+import five from "./assets/five.png";
+import six from "./assets/six.png";
+
+
+class App extends Component {
+  state = {
+  numberOfDice: null,
+  rolls: [],
+  rollSum: null
+};
+
+
+
+
+
+diceRoll = numberOfDice => {
+  let rolls = [];
+  let rollSum = 0;
+  for (let i = 0; i < numberOfDice; i++) {
+    rolls[i] = Math.floor(Math.random() * 6) + 1;
+    rollSum += rolls[i];
+  }
+  this.setState({
+    numberOfDice,
+    rolls,
+    rollSum
+  });
+};
+
+
+  
+  render() {
+    return (
+      <div className="App">
+      {
+         this.state.rolls.map((roll, index) => <DiceImage roll={roll} key={index} />)
+      }
+        <h1>Dice Roll Demo</h1>
+              <div className="buttons">
+              {[1, 2, 3, 4, 5].map(number => {
+                let text = number === 1 ? "die" : "dice";
+                return (
+                  <button
+                    key={number}
+                    onClick={() => this.diceRoll(number)}
+                    className="button"
+                  >
+                    {number} {text}
+                  </button>
+                );
+              })}
+              
+            </div>
+            
+ 
+      </div>
+      
+    );
+  }
 }
+
+const DiceImage = ({ roll }) => {
+  if (roll === 1) {
+    return <img className="dice-image" src={one} alt="1" />;
+  } else if (roll === 2) {
+    return <img className="dice-image" src={two} alt="2" />;
+  } else if (roll === 3) {
+    return <img className="dice-image" src={three} alt="3" />;
+  } else if (roll === 4) {
+    return <img className="dice-image" src={four} alt="4" />;
+  } else if (roll === 5) {
+    return <img className="dice-image" src={five} alt="5" />;
+  } else if (roll === 6) {
+    return <img className="dice-image" src={six} alt="6" />;
+  } 
+  
+  
+};
 
 export default App;
---
+++ dice-roller/src/roller.js
@@ -0,0 +1,3 @@
+class Roller
+
+Math.floor( Math.random() * 6 + 1 )
